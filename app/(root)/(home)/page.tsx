96633499de159d4b1244463d59f108a8208cd413
VSat08 <satyam2001verma@gmail.com>
2024-07-25 22:22:17 +0530

    Fixes: classNames typos

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -13,7 +13,7 @@ const Home = () => {
   return (
     <section className="flex size-full flex-col gap-10 text-white">
       <div className="h-[300px] w-full rounded-2xl bg-hero bg-cover">
-        <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
+        <div className="flex h-full flex-col justify-center p-6 gap-16 max-md:px-5 max-md:py-8 lg:p-11">
           <h2 className="glassmorphism max-w-[270px] rounded py-2 text-center text-base  font-normal">
             Upcoming Meeting at 12:30 PM
           </h2>
