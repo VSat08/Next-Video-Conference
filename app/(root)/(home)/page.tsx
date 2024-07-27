75876842832f3a5d28dd7feccda869d3e67d184c
VSat08 <satyam2001verma@gmail.com>
2024-07-27 17:30:45 +0530

    UI Sparkles added

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -12,18 +12,22 @@ const Home = () => {
 
   return (
     <section className="flex size-full flex-col gap-10 text-white">
-      <div className="h-[300px] w-full rounded-3xl bg-hero bg-cover">
-        <div className="flex h-full flex-col justify-center p-6 gap-16 max-md:px-5 max-md:py-8 lg:p-11">
-          <h2 className="glassmorphism max-w-[270px] rounded-xl py-2 text-center text-base  font-normal">
-            Upcoming Meeting at 12:30 PM
-          </h2>
-          <div className="flex flex-col gap-2 items-start">
-            <h1 className="text-4xl font-extrabold lg:text-7xl">{time}</h1>
-            <p className="glassmorphism px-4 p-1.5 rounded-2xl text-lg font-normal text-neutral-50 lg:text-xl">
-              {date}
-            </p>
+      <div className="flex items-center gap-2 justify-between">
+        <div className="h-[300px] w-full rounded-3xl bg-hero bg-cover">
+          <div className="flex h-full flex-col justify-center p-6 gap-16 max-md:px-5 max-md:py-8 lg:p-11">
+            <h2 className="glassmorphism max-w-[270px] rounded-xl py-2 text-center text-base  font-normal">
+              Upcoming Meeting at 12:30 PM
+            </h2>
+            <div className="flex flex-col gap-2 items-start">
+              <h1 className="text-4xl font-extrabold lg:text-7xl">{time}</h1>
+              <p className="glassmorphism px-4 p-1.5 rounded-2xl text-lg font-normal text-neutral-50 lg:text-xl">
+                {date}
+              </p>
+            </div>
           </div>
         </div>
+
+        <div className="w-1/2 bg-red-200 h-full hidden md:block rounded-3xl">fsdgs</div>
       </div>
 
       <MeetingTypeList />
